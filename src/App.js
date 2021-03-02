929aadcd9be93113aa7b6daf784752c27a98e993
Faran <syed_faran@live.com>
2021-03-02 15:30:35 +0500

    first route done 77

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
+
+const HatsPage = () => (
+  <div>
+    <h1>hatsPage</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
